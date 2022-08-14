e0d5b573ca92de45ab1cc1dc9a1b21ffe1764d36
Olecya <OlecyaMarkina@yandex.ru>
2022-08-14 08:37:17 +0300

    весь проект

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+console.log('работает');
+let popup = document.querySelector('.popup');
+let openButton = document.querySelector('.popap_open-popup');
+let closeButton = popup.querySelector('.popup__close');
+
+let togglePopup = function () {
+    popup.classList.toggle('popup_open-popup');
+}
+
+openButton.addEventListener('click', togglePopup)
+closeButton.addEventListener('click', togglePopup)
+
+
+let formElement = popup.querySelector('.popup__form');
+let popupContent = formElement.querySelector('.popup__content');
+let popupButton = popupContent.querySelector('.popup__button');
+console.log(1);
+ 
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    console.log(2);
+
+    let nameInput = popupContent.querySelector('.popup__name').value;
+    let jobInput = popupContent.querySelector('.popup__info').value;
+
+    let profileTitle = document.querySelector('.profile__info_title');
+    let profileSubtitle = document.querySelector('.profile__info_subtitle');
+  
+    profileTitle.textContent = nameInput;
+    profileSubtitle.textContent = jobInput;
+    popupButton.addEventListener('click', togglePopup);
+}
+popupContent.addEventListener('submit', formSubmitHandler);
+ 
+
+
