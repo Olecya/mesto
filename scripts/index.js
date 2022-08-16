03d00a47f773fd845a547b889387292355a29624
Olecya <OlecyaMarkina@yandex.ru>
2022-08-17 00:32:45 +0300

    ревью1

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,36 @@
-console.log('работает');
 let popup = document.querySelector('.popup');
-let openButton = document.querySelector('.profile__button-open_popup_opened');
+let openButton = document.querySelector('.profile__button-open');
 let closeButton = popup.querySelector('.popup__close');
 
-let togglePopup = function () {
-    popup.classList.toggle('profile__button-open_popup_opened');
+function openPopup () {
+    popup.classList.add('popup_opened');
+   
 }
 
-openButton.addEventListener('click', togglePopup)
-closeButton.addEventListener('click', togglePopup)
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
 
+openButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
 let formElement = popup.querySelector('.popup__form');
 let popupContent = formElement.querySelector('.popup__content');
 let popupButton = popupContent.querySelector('.popup__button');
-console.log(1);
  
 function formSubmitHandler (evt) {
     evt.preventDefault();
     console.log(2);
 
-    let nameInput = popupContent.querySelector('.popup__name').value;
-    let jobInput = popupContent.querySelector('.popup__info').value;
+    let nameInput = popupContent.querySelector('.popup__input_name').value;
+    let jobInput = popupContent.querySelector('.popup__input_info').value;
 
     let profileTitle = document.querySelector('.profile__title');
     let profileSubtitle = document.querySelector('.profile__subtitle');
   
     profileTitle.textContent = nameInput;
     profileSubtitle.textContent = jobInput;
-    popupButton.addEventListener('click', togglePopup);
+    closePopup();
 }
 popupContent.addEventListener('submit', formSubmitHandler);
  
