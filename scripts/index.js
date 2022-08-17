7fa4fc06e2343a4c816b076e1f14bc282d4ee6eb
Olecya <OlecyaMarkina@yandex.ru>
2022-08-18 01:12:16 +0300

    итерация2

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,10 @@ let closeButton = popup.querySelector('.popup__close');
 
 function openPopup () {
     popup.classList.add('popup_opened');
+    let nameInputProfile = document.querySelector('.popup__input_profile_name');
+    let jobInputProfile = document.querySelector('.popup__input_profile_info');
+    nameInputProfile.value = document.querySelector('.profile__title').textContent;
+    jobInputProfile.value = document.querySelector('.profile__subtitle').textContent;
    
 }
 
@@ -20,10 +24,9 @@ let popupButton = popupContent.querySelector('.popup__button');
  
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    console.log(2);
 
-    let nameInput = popupContent.querySelector('.popup__input_name').value;
-    let jobInput = popupContent.querySelector('.popup__input_info').value;
+    let nameInput = popupContent.querySelector('.popup__input_profile_name').value;
+    let jobInput = popupContent.querySelector('.popup__input_profile_info').value;
 
     let profileTitle = document.querySelector('.profile__title');
     let profileSubtitle = document.querySelector('.profile__subtitle');
